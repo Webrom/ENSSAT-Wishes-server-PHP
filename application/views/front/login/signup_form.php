79b659c7aa18b836d31684d6f9ef07791cf69ac1
TheultimateZank <zankowitch@gmail.com>
2015-05-27 08:48:34 +0200

    isncription

--- application/views/front/login/signup_form.php
+++ application/views/front/login/signup_form.php
@@ -18,36 +18,32 @@
         <div class="col-md-4">
             <div class="bp-component">
                 <?php echo form_open('login/createUser','class="form-horizontal"')?>
-<fieldset>
-    <legend>Inscription</legend>
-    <div class="form-group">
-        <div class="col-md-12 col-no-border">
-            <label for="inputEmail" class="2 col-no-border control-label">Nom d'utilisateur</label>
-            <?php echo form_input('username','Username','class="form-control" id="inputEmail" placeholder="Username"')?>
+                    <fieldset>
+                        <legend>Inscription</legend>
+                        <div class="form-group">
+                            <div class="col-md-12 col-no-border">
+                                <label for="name" class="control-label">Votre nom</label>
+                                <?php echo form_input('name','John','class="form-control" placeholder="John" id="name"')?>
+                            </div>
+                            <div class="col-md-12 col-no-border">
+                                <label for="prenom" class="control-label">Votre prenom</label>
+                                <?php echo form_input('prenom','Doe','class="form-control" placeholder="Doe" id="prenom"')?>
+                            </div>
+                            <div class="col-md-12 col-no-border">
+                                <label for="inputPassword" class="control-label">Mot de passe</label>
+                                <?php echo form_input('password','Password','class="form-control" placeholder="password" id="inputPassword"')?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-2">
+                                <?php echo form_submit('submit','Inscription','class="btn btn-success"')?>
+                                <?php echo anchor('login/','Retour','class="btn btn-info"');?>
+                            </div>
+                        </div>
+                    </fieldset>
+                <?php echo form_close();?>
+            </div>
         </div>
-        <div class="col-md-12 col-no-border">
-            <label for="inputPassword" class="control-label">Mot de passe</label>
-            <?php echo form_input('password','Password','class="form-control" placeholder="password" id="inputPassword"')?>
-        </div>
-        <div class="col-md-12 col-no-border">
-            <label for="name" class="control-label">Votre nom</label>
-            <?php echo form_input('name','John','class="form-control" placeholder="John" id="name"')?>
-        </div>
-        <div class="col-md-12 col-no-border">
-            <label for="prenom" class="control-label">Votre prenom</label>
-            <?php echo form_input('prenom','Doe','class="form-control" placeholder="Doe" id="prenom"')?>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-md-10 col-md-offset-2">
-            <?php echo form_submit('submit','Inscription','class="btn btn-success"')?>
-            <?php echo anchor('login/','Retour','class="btn btn-info"');?>
-        </div>
-    </div>
-</fieldset>
-<?php echo form_close();?>
-</div>
-</div>
-<div class="col-md-4 col-no-border"></div>
+    <div class="col-md-4 col-no-border"></div>
 </div>
 </div>
