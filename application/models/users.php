44fc1111f484053b2cb625a4b6c212f61d622b2a
TheultimateZank <zankowitch@gmail.com>
2015-05-27 14:34:25 +0200

    profile 2

--- application/models/users.php
+++ application/models/users.php
@@ -25,11 +25,12 @@ class Users extends CI_Model{
     }
 
     public function getUserData(){
-        $this->db->SELECT ("nom","prenom", "statut", "statutaire");
+        $this->db->SELECT ("nom, prenom, statut, statutaire");
         $this->db->from ("enseignant");
         $this->db->where("login",$this->session->userdata('username'));
         $query =  $this->db->get();
-        return $query->result();
+
+        return $query->result_array();
     }
 
     /**
--- application/views/back/profile/profile_panel.php
+++ application/views/back/profile/profile_panel.php
@@ -8,9 +8,7 @@
             <h1 id="forms">Profile</h1>
         </div>
     </div>
-    <?php foreach($userInfos as $userInfo){
-        var_dump($userInfo);
-    };?>
+    <?php foreach($userInfos as $userInfo):;?>
     <div class="row">
         <div class="col-md-2 col-no-border"></div>
         <div class="col-md-8">
@@ -19,10 +17,26 @@
                     <img class="media-object img-circle" alt="64x64" src="<?php echo base_url()?>assets/img/comments/01.jpg" style="width: 64px; height: 64px;">
                 </div>
                 <div class="col-md-10">
-                    <h2><?php echo "aze";?></h2>
+                    <h2><?php echo $userInfo['nom']." ".$userInfo['prenom'];?></h2>
                 </div>
+            </div>
+            <div class="col-md-6 col-no-border">
+                <div class="col-md-12 col-no-border">
+                    <label for="inputStatut" class="2 col-no-border control-label">Votre statut :</label>
+                    <input class="form-control" id="inputStatut" value="<?php echo $userInfo['statut'];?>" type="text">
+                </div>
+                <div class="col-md-12 col-no-border">
+                    <label for="inputStatutaire" class="2 col-no-border control-label">Total d'heure à effectuer :</label>
+                    <input class="form-control" id="inputStatutaire" value="<?php echo $userInfo['statutaire'];?>" type="text">
+                </div>
+            </div>
+            <div class="col-md-6 col-no-border">
+                <?php echo form_open()?>
+
+                <?php echo form_close()?>
             </div>
         </div>
         <div class="col-md-2 col-no-border"></div>
     </div>
+    <?php endforeach ?>
 </div>
