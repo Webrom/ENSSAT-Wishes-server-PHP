9356ac667dc0e0ea058b1561686669b04ee68de7
TheultimateZank <zankowitch@gmail.com>
2015-05-27 13:15:54 +0200

    modification css

--- application/views/front/login/login_form.php
+++ application/views/front/login/login_form.php
@@ -14,7 +14,7 @@
 
     <div class="row">
         <div class="col-md-4 col-no-border"></div>
-        <div class="col-md-4">
+        <div id="loginForm" class="col-md-4">
             <?php if(isset($msg)):?>
                 <div class="alert alert-dismissable alert-danger">
                     <button type="button" class="close" data-dismiss="alert">×</button>
--- application/views/front/login/signup_form.php
+++ application/views/front/login/signup_form.php
@@ -12,21 +12,25 @@
     </div>
 
     <div class="row">
-        <div class="col-md-4 col-no-border"></div>
-        <div class="col-md-4">
+        <div class="col-md-3 col-no-border"></div>
+        <div class="col-md-6" id="signUpForm">
             <div class="bp-component">
                 <?php echo form_open('login/createUser','class="form-horizontal"')?>
                     <fieldset>
                         <legend>Inscription</legend>
-                        <div class="form-group">
-                            <div class="col-md-12 col-no-border">
-                                <label for="name" class="control-label">Votre nom</label>
-                                <?php echo form_input('name','John','class="form-control" placeholder="John" id="name"')?>
+                        <div class="col-md-6 col-no-border">
+                            <div class="form-group">
+                                <div class="col-md-12 col-no-border">
+                                    <label for="name" class="control-label">Votre nom</label>
+                                    <?php echo form_input('name','John','class="form-control" placeholder="John" id="name"')?>
+                                </div>
+                                <div class="col-md-12 col-no-border">
+                                    <label for="prenom" class="control-label">Votre prenom</label>
+                                    <?php echo form_input('prenom','Doe','class="form-control" placeholder="Doe" id="prenom"')?>
+                                </div>
                             </div>
-                            <div class="col-md-12 col-no-border">
-                                <label for="prenom" class="control-label">Votre prenom</label>
-                                <?php echo form_input('prenom','Doe','class="form-control" placeholder="Doe" id="prenom"')?>
-                            </div>
+                        </div>
+                        <div class="col-md-6 col-no-border">
                             <div class="col-md-12 col-no-border">
                                 <label for="inputPassword" class="control-label">Mot de passe</label>
                                 <?php echo form_input('password','Password','class="form-control" placeholder="password" id="inputPassword"')?>
@@ -34,7 +38,7 @@
                             <div class="col-md-12 col-no-border">
                                 <label for="select_statut" class="control-label">Votre statut</label>
                                 <?php foreach ($status as $lestatut){
-                                echo "$lestatut->statut <br />";
+                                    echo "$lestatut->statut <br />";
                                 }?>
                                 <?php echo form_input('name','John','class="form-control" placeholder="John" id="name"')?>
                             </div>
@@ -49,6 +53,6 @@
                 <?php echo form_close();?>
             </div>
         </div>
-    <div class="col-md-4 col-no-border"></div>
+    <div class="col-md-3 col-no-border"></div>
 </div>
 </div>
