8c61983e7aeb0e7579ed819fa8715b9d7775bb7b
TheultimateZank <zankowitch@gmail.com>
2015-05-27 14:40:31 +0200

    profile 2

--- application/views/back/profile/profile_panel.php
+++ application/views/back/profile/profile_panel.php
@@ -23,11 +23,11 @@
             <div class="col-md-6 col-no-border">
                 <div class="col-md-12 col-no-border">
                     <label for="inputStatut" class="2 col-no-border control-label">Votre statut :</label>
-                    <input class="form-control" id="inputStatut" value="<?php echo $userInfo['statut'];?>" type="text">
+                    <input disabled class="form-control" id="inputStatut" value="<?php echo $userInfo['statut'];?>" type="text">
                 </div>
                 <div class="col-md-12 col-no-border">
                     <label for="inputStatutaire" class="2 col-no-border control-label">Total d'heure à effectuer :</label>
-                    <input class="form-control" id="inputStatutaire" value="<?php echo $userInfo['statutaire'];?>" type="text">
+                    <input disabled class="form-control" id="inputStatutaire" value="<?php echo $userInfo['statutaire'];?>" type="text">
                 </div>
             </div>
             <div class="col-md-6 col-no-border">
