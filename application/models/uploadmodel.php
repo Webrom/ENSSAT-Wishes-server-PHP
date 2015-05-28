e0e9255185f0ecae4b2e7c632c35c2435cf1013c
Colin LEVERGER <colinleverger@gmail.com>
2015-05-28 11:02:33 +0200

    LA BONNE !!!!!!!! ALTER TABLE enseignant ADD avatar VARCHAR(30)

--- application/models/uploadmodel.php
+++ application/models/uploadmodel.php
@@ -6,9 +6,13 @@
  * Time: 10:47
  */
 class uploadmodel extends CI_Model{
-
+    //ALTER TABLE enseignant ADD path_avatar VARCHAR(255)
     public function changeAvatar($upload_data){
-
+        $data = array(
+            'path_avatar' => $upload_data[],
+        );
+        $this->db->where('login', $userName);
+        $this->db->update('enseignant', $data);
     }
 }
 ?>
