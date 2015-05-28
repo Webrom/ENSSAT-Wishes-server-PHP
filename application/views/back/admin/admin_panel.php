ea7f59de5ebdfeaec1c99b5518472aee0ef2f53b
TheultimateZank <zankowitch@gmail.com>
2015-05-28 18:09:29 +0200

    suppression module v0.1

--- application/views/back/admin/admin_panel.php
+++ application/views/back/admin/admin_panel.php
@@ -9,16 +9,20 @@
 ?>
 <div class="bp-docs-section">
     <div class="row">
-        <div class="col-md-12 title-section">
-            <h1>administration</h1>
-        </div>
     </div>
     <div class="row">
         <div class="col-md-2 col-no-border"></div>
         <div class="col-md-8">
+            <?php if($msg!=null):?>
+            <div class="alert alert-dismissable <?php echo $success?>">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <?php echo $msg ?>
+            </div>
+            <?php endif;?>
             <div class="bp-component">
                 <ul class="nav nav-tabs" style="margin-bottom: 15px;">
                     <li class="active"><a href="#module" data-toggle="tab">Modules</a></li>
+                    <li><a href="#contenu" data-toggle="tab">Contenu</a></li>
                     <li><a href="#profile" data-toggle="tab">Utilisateurs</a></li>
                     <li><a href="#news" data-toggle="tab">News</a></li>
                 </ul>
@@ -80,11 +84,24 @@
                             <div class="col-md-6 col-no-border">
                                 <div class="bp-component">
                                     <div class="col-md-12 col-no-border">
-                                        <?php echo form_open("",'class="form-horizontal"')?>
+                                        <?php echo form_open("admin/deleteModule",'class="form-horizontal"')?>
                                         <fieldset>
                                             <legend>Supprimer un module</legend>
                                             <div class="form-group">
-
+                                                <div class="col-md-12 col-no-border">
+                                                    <label for="selectModule" class="control-label">Selectionnez Module</label>
+                                                    <select name="module[]" class="form-control" id="selectModule" multiple style="height: 380.5px">
+                                                        <?php foreach($modules as $module):?>
+                                                            <option value="<?php echo $module['ident'];?>"><?php echo $module['ident'];?></option>
+                                                        <?php endforeach;?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12 col-no-border">
+                                                <div class="col-md-8 col-no-border"></div>
+                                                <div class="col-md-4 col-no-border">
+                                                    <?php echo form_submit('submit','valider','class="btn btn-success"')?>
+                                                </div>
                                             </div>
                                         </fieldset>
                                         <?php echo form_close()?>
@@ -147,6 +164,38 @@
                                         <?php echo form_open("",'class="form-horizontal"')?>
                                         <fieldset>
                                             <legend>Supprimer une news</legend>
+                                            <div class="form-group">
+
+                                            </div>
+                                        </fieldset>
+                                        <?php echo form_close()?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="contenu">
+                        <div class="row">
+                            <div class="col-md-6 col-no-border">
+                                <div class="bp-component">
+                                    <div class="col-md-12 col-no-border">
+                                        <?php echo form_open("",'class="form-horizontal"')?>
+                                        <fieldset>
+                                            <legend>Ajouter un contenu à un module</legend>
+                                            <div class="form-group">
+
+                                            </div>
+                                        </fieldset>
+                                        <?php echo form_close()?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-no-border">
+                                <div class="bp-component">
+                                    <div class="col-md-12 col-no-border">
+                                        <?php echo form_open("",'class="form-horizontal"')?>
+                                        <fieldset>
+                                            <legend>Supprimer un contenu d'un module</legend>
                                             <div class="form-group">
 
                                             </div>
