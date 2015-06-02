4b7eb71b2f19201cc482c0bd80d53aac6472e65e
TheultimateZank <zankowitch@gmail.com>
2015-06-02 10:38:18 +0200

    progress bar menu faudra voir si on peut pas mettre le calcul du pourcentage en factorisé ajout homenews

--- application/controllers/homeNews.php
+++ application/controllers/homeNews.php
@@ -7,6 +7,14 @@
  */
 
 class homeNews extends CI_Controller {
+    function __construct(){
+        parent::__construct();
+        //charger le modele users
+        $this->load-> model('users');
+        $this->load-> model('contenu');
+        $this->load-> model('decharge');
+    }
+    
     public function index(){
         if(!$this->session->userdata('is_logged_in')){
             redirect('login');
@@ -14,6 +22,16 @@ class homeNews extends CI_Controller {
             $data = array(
                 "admin" => $this->session->userdata['admin'],
             );
+
+            /* CALCUL POURCENTAGE HEURES PRISES */
+            $heuresprises = $this->contenu->getHeuresPrises($this->session->userdata('username'));
+            $heurestotales = $this->users->getHeures()-$this->decharge->getHoursDecharge($this->session->userdata('username'));
+            $pourcentage = round(($heuresprises/$heurestotales)*100,0);
+            $data['pourcentage'] = $pourcentage;
+            $data['heuresprises'] = $heuresprises;
+            $data['heurestotales'] = $heurestotales;
+            /* FIN CALCUL */
+
             $this->load->view('header',$data);
             $this->load->view('back/template/header');
             $this->load->view('footer');
