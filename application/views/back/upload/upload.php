ded5c32002da669df9fe842317814325896cf279
TheultimateZank <zankowitch@gmail.com>
2015-06-11 11:55:06 +0200

    custom box img upload

--- application/views/back/upload/upload.php
+++ application/views/back/upload/upload.php
@@ -10,16 +10,12 @@
     <div class="row clearfix"></div>
     <div class="row">
         <div class="col-md-4 col-no-border"></div>
-        <div class="col-md-4">
-            <div class="col-md-12 col-no-border">
+        <div class="col-md-4 custom-box">
+            <div class="row">
                 <?php echo $error;?>
             </div>
-            <div class="col-md-12 col-no-border">
-                <div class="col-md-4 col-no-border"></div>
-                    <div class="col-md-6 col-no-border">
-                        <?php echo anchor('profile','retour','class="btn btn-info"');?>
-                    </div>
-                <div class="col-md-4 col-no-border"></div>
+            <div class="row text-center">
+                <?php echo anchor('profile','retour','class="btn btn-info"');?>
             </div>
         </div>
         <div class="col-md-4 col-no-border"></div>
