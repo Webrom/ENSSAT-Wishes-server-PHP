5082e3fb3f4a1ac5f9f8fbff68dcaf38c4e536f3
TheultimateZank <zankowitch@gmail.com>
2015-06-21 20:09:25 +0200

    OMG PUSH FINAL DE SES MORTS J'AI MON CLES @ColinLeverger

--- application/views/front/template/footer_help.php
+++ application/views/front/template/footer_help.php
@@ -31,4 +31,50 @@
         </div>
     </div>
 </div>
+</div>
+    <div class="alert alert-success alert-dismissible fade in text-center" role="alert" style="z-index:10000;position: fixed; bottom: 0; width: 100%; margin-bottom: 0">
+        <strong>Cookies :</strong> Ce site utilise des cookies pour ses services.
+        <a href="#" data-toggle="modal" data-target=".bp-cookie-modal-sm">En savoir plus</a>
+        <button type="button" class="btn btn-success" onclick="C.createCookie(C.cookieName, C.cookieValue, C.cookieDuration)" data-dismiss="alert" aria-label="Close">OK</button>
+    </div>
+<div class="modal fade bp-cookie-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Cookies</h4>
+            </div>
+            <div class="modal-body">
+                <div class="mainContentBlock section sectionMain">
+                    <div class="baseHtml">
+                        <h3 class="textHeading">Que sont les cookies ?</h3>
+                        <p>Les cookies sont de petits fichiers stockés sur votre ordinateur par votre navigateur (comme Internet Explorer ou Firefox), à la demande d'un site que vous consultez. Cela permet au site que vous consultez de se souvenir de choses sur vous, comme vos préférences, votre historique ou de vous garder connecté.</p>
+
+                        <p>Les cookies peuvent être stockés sur votre ordinateur pendant une courte période (par exemple, uniquement lorsque votre navigateur est ouvert) ou pendant une période de temps prolongée, voire des années. Les cookies, non créés par ce site, ne nous sont pas accessible.</p>
+
+                        <h3 class="textHeading">Utilisation des cookies</h3>
+                        <p>Ce site utilise des cookies pour de nombreuses choses, notamment: <!-- p-->
+                        </p>
+                        <ul>
+                            <li>Inscription. Cela inclut la possibilité de rester connecté. </li>
+                            <h3 class="textHeading">Suppression/Désactivation de cookies</h3>
+                            <p>Le contrôle des cookies et des préférences doivent être effectués dans votre navigateur. Voici une liste de guides sur la façon de procéder pour les navigateurs populaires : <!-- p-->
+                            </p>
+                        </ul>
+                        <ul>
+                            <li> <a href="http://support.microsoft.com/kb/196955" target="_blank"> Internet Explorer <!-- a--></a></li><a href="http://support.microsoft.com/kb/196955" target="_blank">
+                            </a><li><a href="http://support.microsoft.com/kb/196955" target="_blank"> </a><a href="http://support.mozilla.com/en-US/kb/Cookies" target="_blank"> Firefox <!-- a--> </a></li><a href="http://support.mozilla.com/en-US/kb/Cookies" target="_blank">
+                            </a><li><a href="http://support.mozilla.com/en-US/kb/Cookies" target="_blank"> </a><a href="http://www.google.com/support/chrome/bin/answer.py?hl=en&amp;answer=95647" target="_blank"> Chrome <!-- a--> </a></li><a href="http://www.google.com/support/chrome/bin/answer.py?hl=en&amp;answer=95647" target="_blank">
+                            </a><li><a href="http://www.google.com/support/chrome/bin/answer.py?hl=en&amp;answer=95647" target="_blank"> </a><a href="http://support.apple.com/kb/PH5042" target="_blank"> Safari <!-- a--> </a></li><a href="http://support.apple.com/kb/PH5042" target="_blank">
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info" data-dismiss="modal">Fermer</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
 
